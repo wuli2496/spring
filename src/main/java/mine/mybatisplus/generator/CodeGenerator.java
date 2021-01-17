e3c718f481de5ba6701f750c6297282550478e77
wuli2496 <wuli2496@163.com>
2021-01-17 20:49:59 +0800

    修改代码生成器

--- src/main/java/mine/mybatisplus/generator/CodeGenerator.java
+++ src/main/java/mine/mybatisplus/generator/CodeGenerator.java
@@ -39,13 +39,14 @@ public class CodeGenerator {
         globalConfig.setOutputDir(projectPath + "/src/main/java");
         globalConfig.setAuthor("wuli");
         globalConfig.setOpen(false);
+        globalConfig.setServiceName("%Service");
         autoGenerator.setGlobalConfig(globalConfig);
 
         DataSourceConfig dataSourceConfig = new DataSourceConfig();
         dataSourceConfig.setUrl("jdbc:mysql://localhost:3306/test?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=GMT");
         dataSourceConfig.setDriverName("com.mysql.cj.jdbc.Driver");
         dataSourceConfig.setUsername("root");
-        dataSourceConfig.setPassword("");
+        dataSourceConfig.setPassword("336699");
         autoGenerator.setDataSource(dataSourceConfig);
 
         final PackageConfig packageConfig = new PackageConfig();
@@ -53,40 +54,14 @@ public class CodeGenerator {
         packageConfig.setParent("com.baomidou.ant");
         autoGenerator.setPackageInfo(packageConfig);
 
-        InjectionConfig injectionConfig = new InjectionConfig() {
-            @Override
-            public void initMap() {
-
-            }
-        };
-
-        String templatePath = "/templates/mapper.xml.ftl";
-
-        List<FileOutConfig> fileOutConfigList = Lists.newArrayList();
-        fileOutConfigList.add(new FileOutConfig(templatePath) {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                return projectPath + "/src/main/resources/mapper/" + packageConfig.getModuleName()
-                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-            }
-        });
-
-        injectionConfig.setFileOutConfigList(fileOutConfigList);
-        autoGenerator.setCfg(injectionConfig);
-
-        TemplateConfig templateConfig = new TemplateConfig();
-        templateConfig.setXml(null);
-        autoGenerator.setTemplate(templateConfig);
-
         StrategyConfig strategyConfig = new StrategyConfig();
         strategyConfig.setNaming(NamingStrategy.underline_to_camel);
         strategyConfig.setColumnNaming(NamingStrategy.underline_to_camel);
-        //strategyConfig.setSuperEntityClass("")
         strategyConfig.setEntityLombokModel(true);
         strategyConfig.setRestControllerStyle(true);
         strategyConfig.setControllerMappingHyphenStyle(true);
+        strategyConfig.setInclude("video");
         autoGenerator.setStrategy(strategyConfig);
-        autoGenerator.setTemplateEngine(new FreemarkerTemplateEngine());
         autoGenerator.execute();
     }
 }
